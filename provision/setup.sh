7792e330aa69babb30045f86403d2431434f9af0
Shanu M <shanu@IN-CH-Shanu.local>
2017-03-27 22:24:38 +0530

    Shanu: Vagrant is ready

---
+++ provision/setup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "Installing Mongo DB"
+cat <<EOT >  /etc/yum.repos.d/mongodb-org-3.4.repo
+[mongodb-org-3.4]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/3.4/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
+EOT
+yum install -y mongodb-org
+
